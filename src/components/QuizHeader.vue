bda70717ca4f923080ddd45ef50bdb2515cc0b1d
bogeeymft <123201375+bogeeymft@users.noreply.github.com>
2023-04-29 14:42:40 +0300

    first commit

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import {defineProps} from "vue"
+    const {
+        questionStatus,
+        barPercentage
+    } = defineProps([
+        "questionStatus",
+        "barPercentage"
+    ])
+</script>
+
+<template>
+    <header>
+        <h4>Soru {{questionStatus}}</h4>
+        <div class="bar">
+            <div class="completion" :style="{width: barPercentage}"></div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        margin-top: 20px
+    }
+    header h4 {
+        font-size: 30px;
+    }
+    .bar {
+        width: 300px;
+        height: 50px;
+        border: 3px solid rgb(25, 200, 101) 
+    }
+    .completion {
+        height: 100%;
+        width: 0%;
+        background-color: rgb(22, 88, 13);
+    }
+</style>
